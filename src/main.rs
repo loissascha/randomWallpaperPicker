87422343ef5480b1cbaa8800d856d417952967a5
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-06-28 15:31:00 +0200

    added clap for argument management

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,29 @@ use std::thread::sleep;
 use std::time::Duration;
 use walkdir::WalkDir;
 use rand::prelude::*;
+use clap::Parser;
+
+/// Random Wallpaper picker using swww
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path of your wallpaper directory
+    #[arg(short, long)]
+    path: String,
+
+    /// Seconds between wallpaper changes
+    #[arg(short, long, default_value_t = 300)]
+    duration: u64,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args = Args::parse();
     let mut rng = thread_rng();
-    let mut file_path:&str;
-
-    match args.len() {
-        0 | 1 => {
-            println!("No argument was supplied");
-            return;
-        },
-        _ => {
-            file_path = &args[1];
-        }
-    }
 
     loop {
         let mut files = Vec::new();
 
-        for entry in WalkDir::new(file_path) {
+        for entry in WalkDir::new(&args.path) {
             let entry = entry.unwrap();
             if entry.file_type().is_file() {
                 files.push(entry.path().to_owned());
@@ -41,6 +44,6 @@ fn main() {
             println!("No files found");
         }
 
-        sleep(Duration::from_secs(300));
+        sleep(Duration::from_secs(args.duration));
     }
 }
